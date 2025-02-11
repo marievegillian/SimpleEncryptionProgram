65b448d9c831a5eac9dc508a54ed3e8e1c86a0e2
Marieve <marievegillian@gmail.com>
2025-02-11 09:26:14 +0800

    Add project files.

---
+++ Aldemita_Act2/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aldemita_Act2
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog1;
+        private string macAddress;
+
+        public Form1()
+        {            
+            InitializeComponent();
+            macAddress = GetMacAddress()?.ToLower();
+        }
+
+        private string GetMacAddress()
+        {
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            return networkInterfaces
+                .Where(nic => nic.OperationalStatus == OperationalStatus.Up)
+                .Select(nic => nic.GetPhysicalAddress().ToString())
+                .FirstOrDefault();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                int[] temp = new int[fs.Length];
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] = fs.ReadByte();
+                }
+                fs.Close();
+
+                fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate);
+                int macLength = macAddress.Length;
+                for (int y = 0; y < macLength; y++)
+                {                    
+                    fs.WriteByte((byte)(temp[y] + macAddress.ElementAt(y)));
+                }
+                fs.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                int[] temp = new int[fs.Length];
+
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    temp[i] = fs.ReadByte();
+                }
+                fs.Close();
+
+                fs = new FileStream(openFileDialog1.FileName, FileMode.OpenOrCreate);
+                int macLength = macAddress.Length;
+                for (int y = 0; y < macLength; y++)
+                {
+                    fs.WriteByte((byte)(temp[y] - macAddress.ElementAt(y)));
+                }
+                fs.Close();
+            }
+        }
+    }
+}
